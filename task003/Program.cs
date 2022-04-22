14a20dddd329893a13b10bcc5c048a531726bb99
dmitry_di <dnd_d@mail.ru>
2022-04-22 14:19:17 +0300

    Написали программу, которая выводит произведение чисел от 1 до N

---
+++ task003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3. Написать программу, которая на входе принимает число N, а на выходе выдает произведение чисел от 1 до N.
+using System;
+Console.Clear();
+Console.Write("Введите число N: ");
+Console.WriteLine(multiplication(int.Parse(Console.ReadLine())));
+
+int multiplication(int number)
+{
+    int multi=1;
+    for(int i=1; i<= number; i++)
+    {
+        multi = multi * i;
+    }
+    return multi;
+}
