7fb82ec55bf44c0afed8bd0cf7f484bb453dd69a
dmitry_di <dnd_d@mail.ru>
2022-04-22 15:19:39 +0300

    Заполнение массива длиной К 0-ми и 1-ми

---
+++ task005/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №4 Заполнить массив из k элементов нулями и единицами (0 и 1) 
+using System;
+Console.Clear();
+Console.Write("Введите длину массива K: ");
+int K = int.Parse(Console.ReadLine());
+Console.WriteLine(string.Join(" ", array(K)));
+
+
+int[] array(int L)
+{
+    int[] arr = new int[L];
+
+    for (int i = 0; i < L; i++)
+    {
+        arr[i] = new Random().Next(2);
+    }
+    return arr;
+}
