5e20f9277840ed7b806ed510498fedb1647d5c68
dmitry_di <dnd_d@mail.ru>
2022-04-22 10:33:46 +0300

    Написали метод подсчета суммы чисел от 1 до A

---
+++ task001/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1. Написать программу, которая на входе принимает число А, а на выходе выдает суммы чисел от 1 до А.
+/*Вариант 1.
+using System;
+Console.Clear();
+Console.Write("Введите А: ");
+int A=int.Parse(Console.ReadLine());
+Console.WriteLine(Summa(A));
+
+int Summa(int number)
+{
+    int sum = 0;
+    for(int i = 0; i<=number; i++)
+    {
+        sum +=i;
+    }
+    return sum;
+}*/
+//Вариант 2. Убираем необходимость ввода дополнительной переменной А.
+using System;
+Console.Clear();
+Console.Write("Введите А: ");
+//int A=int.Parse(Console.ReadLine());
+Console.WriteLine(Summa(int.Parse(Console.ReadLine())));
+
+int Summa(int number)
+{
+    int sum = 0;
+    for(int i = 0; i<=number; i++)
+    {
+        sum +=i;
+    }
+    return sum;
+}
