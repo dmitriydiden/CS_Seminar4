ec0805983a79b4bc008f6ef29ebbf604e254eea1
dmitry_di <dnd_d@mail.ru>
2022-04-22 15:00:04 +0300

    Заполнили массив из 8-ми элементов 0-ми и 1-ми

---
+++ task004/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача №4 Заполнить массив из 8 элементов нулями и единицами (0 и 1) 
+using System;
+Console.Clear();
+int[] array = new int[8];
+for (int i=0; i<array.Length; i++)
+{
+    array[i]=new Random().Next(0, 2);
+    Console.Write(array[i] + " ");
+}
