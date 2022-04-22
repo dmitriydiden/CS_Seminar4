4f95d17cb9b6faaed16b3d7b65275dc6a029c2ba
dmitry_di <dnd_d@mail.ru>
2022-04-22 13:49:12 +0300

    Написали программу, которая считает количество цифр в числе

---
+++ task002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №2. Написать программу, которая на вход принимает число выдает количество цифр в числе
+using System;
+Console.Clear();
+Console.Write("Input number A: ");
+Console.WriteLine($"В числе А - {amount(int.Parse(Console.ReadLine()))} цифр/ы.");
+
+int amount(int number)
+{
+    int am = 1;
+    for (int i = 10; number/i > 0; i = i * 10)
+    {
+        am++;
+    }
+    return am;
+}
