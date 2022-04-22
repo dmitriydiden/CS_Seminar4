f2807dbacadcf7e0933edf29b246c76668e53359
dmitry_di <dnd_d@mail.ru>
2022-04-22 09:59:42 +0300

    Написали метод, который выводит массив в строку

---
+++ Metod_output_array/Program.cs
@@ -0,0 +1,8 @@
+﻿// Метод вывода массива в строку с задаваемыми отступами
+Console.Clear();
+int[] array = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    array[i] = new Random().Next(10);
+}
+Console.WriteLine(String.Join(" ", array));
